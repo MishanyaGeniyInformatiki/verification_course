85468aebf1019ff73aa09a8247ea4c49e58e391b
mikhail <m.solovev@phystech.edu>
2024-09-18 19:20:46 +0300

    add attributes for classes

--- src/customer.py
+++ src/customer.py
@@ -1,5 +1,18 @@
 class Cart:
-    pass
+    def __init__(self, cart_id):
+        self.cart_id = cart_id
+        self.books = []
+
+    def add_book_to_cart(self, book):
+        pass
+
 
 class Customer:
-    pass  
+    def __init__(self, customer_id, name):
+        self.customer_id = customer_id
+        self.name = name
+        self.cart = Cart(cart_id=customer_id)
+        self.orders = []
+
+    def add_book_to_cart(self, book):
+        pass
