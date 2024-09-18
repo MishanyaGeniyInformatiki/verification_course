3ffbb098a310627193832d35f3333391fb0540ac
mikhail <m.solovev@phystech.edu>
2024-09-18 18:53:24 +0300

    add test_list_books in test_bookstore.py for TDD

--- tests/test_bookstore.py
+++ tests/test_bookstore.py
@@ -6,6 +6,26 @@ class TestBookStore(unittest.TestCase):
 
     def setUp(self):
         self.store = BookStore()
+        self.book1 = Book(
+            1,
+            "To Kill a Mockingbird",
+            "Harper Lee",
+            1960,
+            10.99,
+            "J.B. Lippincott & Co.",
+            "Fiction",
+        )
+        self.book2 = Book(
+            2,
+            "1984",
+            "George Orwell",
+            1949,
+            8.99,
+            "Secker & Warburg",
+            "Dystopian",
+        )
+        self.store.add_book(self.book1)
+        self.store.add_book(self.book2)
 
     def test_add_book(self):
         new_book = Book(
@@ -19,6 +39,12 @@ class TestBookStore(unittest.TestCase):
         )
         self.store.add_book(new_book)
         self.assertIn(new_book, self.store.books)
+
+    def test_list_books(self):
+        self.store.list_books()
+        store_books = [book.title for book in self.store.books]
+        self.assertIn(self.book1.title, store_books)
+        self.assertIn(self.book2.title, store_books)
 
 
 if __name__ == "__main__":
